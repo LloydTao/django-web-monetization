1d087cba04942e84746dab0f4d27fba8a4933597
LloydTao <LL23@live.co.uk>
2020-08-03 00:05:15 +0100

    ✨ Create package setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="django-web-monetization",
+    version="0.1",
+    packages=["monetization", "monetization.migrations"],
+    url="https://github.com/LloydTao/django-web-monetization",
+    license="MIT",
+    author="Lewis Lloyd",
+    author_email="Lewis_Lloyd@live.co.uk",
+    description="A simple integration for the Web Monetization API.",
+)
