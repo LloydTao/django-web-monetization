b98785860bf46ed95e8e9b97d5155bbcf5fe9ea5
LloydTao <LL23@live.co.uk>
2020-08-03 00:43:51 +0100

    ✨ Add README to PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="django-web-monetization",
-    version="0.1",
+    version="0.2",
     packages=["monetization", "monetization.migrations"],
     url="https://github.com/LloydTao/django-web-monetization",
     license="MIT",
     author="Lewis Lloyd",
     author_email="Lewis_Lloyd@live.co.uk",
     description="A simple integration for the Web Monetization API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
